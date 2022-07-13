80509e31348a1463dd68e1dbe92e626fa9248f3f
Kay McCreedy <thiskieranmccreedy@gmail.com>
2022-07-13 00:55:46 -0400

    updates movie-app

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  has_many :actors
   validates :title, length: { minimum: 2 }
   validates :year, numericality: { greater_than: 1887 }
   validates :plot, length: { minimum: 5 }
---
+++ db/migrate/20220712230121_adds_movie_id_to_actors.rb
@@ -0,0 +1,5 @@
+class AddsMovieIdToActors < ActiveRecord::Migration[7.0]
+  def change
+    add_column :actors, :movie_id, :integer
+  end
+end
