b56f2f6f03779c487e30d908d38d5ee2fd6703d3
Kay McCreedy <thiskieranmccreedy@gmail.com>
2022-07-18 19:42:05 -0400

    adds serializer, moviegenres, associations

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,18 @@
 class Movie < ApplicationRecord
   has_many :actors
+  has_many :movie_genres
+  has_many :genres, through: :movie_genres
   validates :title, length: { minimum: 2 }
   validates :year, numericality: { greater_than: 1887 }
   validates :plot, length: { minimum: 5 }
+
+  def genre_names
+    genre_array = []
+    genres.each do |genre|
+      genre_array << genre.name
+    end
+    p genre_array
+  end
+
 
 end
