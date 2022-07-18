61d3e1787eda1b1c6e24cdeb90a5b6d00e6e59ae
Kay McCreedy <thiskieranmccreedy@gmail.com>
2022-07-18 11:46:53 -0400

    adds authentication, authorization, and requests to movies-app

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+
+  def create
+    user = User.new(
+      name: params["name"],
+      email: params["email"],
+      password: params["password"],
+      password_confirmation: params["password_confirmation"],
+      admin: params["admin"]
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def update
+    user = User.find_by(id: params["id"])
+    user.name = params["name"] || user.name
+    user.email = params["email"] || user.email
+    user.admin = params["admin"] || user.admin
+    user.save
+    render json: user.as_json
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -24,4 +24,12 @@ Rails.application.routes.draw do
 
   delete "/movies/:id" => "movies#destroy"
 
+  # Users
+
+  post "/users" => "users#create"
+
+  # Sessions
+
+  post "/sessions" => "sessions#create"
+
 end
